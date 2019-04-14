b07b0e9f1dec44082d7ce7c7810406d74b9fabbc
Filipe Ferreira <filipesferreira94@gmail.com>
2019-04-14 19:03:03 +0100

    First Commit

---
+++ src/Utilizador.java
@@ -0,0 +1,36 @@
+
+public class Utilizador {
+    String nome;
+    String estado;
+    TipoUtilizador tipoUtilizador;
+
+    public Utilizador(String nome, String estado, TipoUtilizador tipoUtilizador) {
+        this.nome = nome;
+        this.estado = estado;
+        this.tipoUtilizador = tipoUtilizador;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public TipoUtilizador getTipoUtilizador() {
+        return tipoUtilizador;
+    }
+
+    public void setTipoUtilizador(TipoUtilizador tipoUtilizador) {
+        this.tipoUtilizador = tipoUtilizador;
+    }
+}
